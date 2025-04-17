93e202ead98e4d8ed272576323f97b67e2345d63
Oleksandr Kurin <profysss8712@gmail.com>
2025-04-17 21:02:39 +0300

    Iplement all CRUD views with search and authÐ

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,153 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
+from django.db.models import Q
+from .models import Newspaper, Redactor, Topic
+from .forms import (
+    NewspaperForm,
+    RedactorCreationForm,
+    RedactorUpdateForm,
+    TopicForm,
+    NewspaperSearchForm,
+    RedactorSearchForm,
+    TopicSearchForm,
+)
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = "newspaper/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["num_newspapers"] = Newspaper.objects.count()
+        context["num_redactors"] = Redactor.objects.count()
+        context["num_topics"] = Topic.objects.count()
+        return context
+
+
+class NewspaperListView(generic.ListView):
+    model = Newspaper
+    paginate_by = 5
+    template_name = "newspaper/newspaper_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        title = self.request.GET.get("title", "")
+        context["search_form"] = NewspaperSearchForm(initial={"title": title})
+        return context
+
+    def get_queryset(self):
+        queryset = Newspaper.objects.select_related("topic").prefetch_related("publishers")
+        form = NewspaperSearchForm(self.request.GET)
+        if form.is_valid() and form.cleaned_data["title"]:
+            return queryset.filter(title__icontains=form.cleaned_data["title"])
+        return queryset
+
+
+class NewspaperDetailView(generic.DetailView):
+    model = Newspaper
+    template_name = "newspaper/newspaper_detail.html"
+
+
+class NewspaperCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Newspaper
+    form_class = NewspaperForm
+    template_name = "newspaper/newspaper_form.html"
+    success_url = reverse_lazy("newspaper:newspaper-list")
+
+
+class NewspaperUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Newspaper
+    form_class = NewspaperForm
+    template_name = "newspaper/newspaper_form.html"
+    success_url = reverse_lazy("newspaper:newspaper-list")
+
+
+class NewspaperDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Newspaper
+    template_name = "newspaper/newspaper_confirm_delete.html"
+    success_url = reverse_lazy("newspaper:newspaper-list")
+
+
+class TopicListView(generic.ListView):
+    model = Topic
+    paginate_by = 5
+    template_name = "newspaper/topic_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = TopicSearchForm(initial={"name": name})
+        return context
+
+    def get_queryset(self):
+        queryset = Topic.objects.prefetch_related("newspapers")
+        form = TopicSearchForm(self.request.GET)
+        if form.is_valid() and form.cleaned_data["name"]:
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
+
+
+class TopicCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Topic
+    form_class = TopicForm
+    template_name = "newspaper/topic_form.html"
+    success_url = reverse_lazy("newspaper:topic-list")
+
+
+class TopicUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Topic
+    form_class = TopicForm
+    template_name = "newspaper/topic_form.html"
+    success_url = reverse_lazy("newspaper:topic-list")
+
+
+class TopicDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Topic
+    template_name = "newspaper/topic_confirm_delete.html"
+    success_url = reverse_lazy("newspaper:topic-list")
+
+
+class RedactorListView(generic.ListView):
+    model = Redactor
+    paginate_by = 5
+    template_name = "newspaper/redactor_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = RedactorSearchForm(initial={"username": username})
+        return context
+
+    def get_queryset(self):
+        queryset = Redactor.objects.all()
+        form = RedactorSearchForm(self.request.GET)
+        if form.is_valid() and form.cleaned_data["username"]:
+            return queryset.filter(username__icontains=form.cleaned_data["username"])
+        return queryset
+
+
+class RedactorDetailView(generic.DetailView):
+    model = Redactor
+    template_name = "newspaper/redactor_detail.html"
+
+
+class RedactorCreateView(generic.CreateView):
+    model = Redactor
+    form_class = RedactorCreationForm
+    template_name = "newspaper/redactor_form.html"
+    success_url = reverse_lazy("newspaper:redactor-list")
+
+
+class RedactorUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Redactor
+    form_class = RedactorUpdateForm
+    template_name = "newspaper/redactor_form.html"
+    success_url = reverse_lazy("newspaper:redactor-list")
+
+
+class RedactorDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Redactor
+    template_name = "newspaper/redactor_confirm_delete.html"
+    success_url = reverse_lazy("newspaper:redactor-list")
+
