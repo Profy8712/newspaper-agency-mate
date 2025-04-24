f48f8110dc47fc7b4f24d196371a457d033120d3
Oleksandr Kurin <profysss8712@gmail.com>
2025-04-24 13:14:07 +0300

    delete secret key from settings.py

--- newspaper_agency_mate/settings.py
+++ newspaper_agency_mate/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY","django-insecure-p&%e38g@*(utxa@#&d(cocub6e&^4-dg+%ow$z76y+na%h_w0%")
+SECRET_KEY = ("django-insecure-p&%e38g@*(utxa@#&d(cocub6e&^4-dg+%ow$z76y+na%h_w0%")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
