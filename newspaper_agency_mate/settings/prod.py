c8825a39a603a44c2c05b4b28d25390213a90bb5
Oleksandr Kurin <profysss8712@gmail.com>
2025-05-01 19:08:18 +0300

    add file build.sh

--- newspaper_agency_mate/settings/prod.py
+++ newspaper_agency_mate/settings/prod.py
@@ -2,7 +2,12 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]  # Replace with your actual domain
+ALLOWED_HOSTS = []  # Replace with your actual domain
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 DATABASES = {
     "default": {
